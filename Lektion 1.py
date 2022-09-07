6cea9ae426bc0cfb1999d9fc2896a2e8e3a4c3db
Ahmed Mahdi <ahmed.mahdi@elev.ga.ntig.se>
2022-09-07 11:02:18 +0200

    Frågesport

---
+++ Lektion 1.py
@@ -0,0 +1,35 @@
+score = 0
+
+ögon = input("vilken färg är dina ögon?").capitalize()
+if ögon == ("Brun"):
+    print ("helt rätt")
+    score +=2
+
+else: print("tyvärr fel")
+
+huvudstad = input("vad är sveriges huvudstad?").capitalize()
+if huvudstad == ("Stockholm"):
+    print ("helt rätt")
+    score +=2
+else: print("tyvärr fel")
+
+gymnasiet = input("vilket gymnasiet går du?").capitalize()
+if gymnasiet == ("Nti"):
+    print ("helt rätt")
+    score +=2 
+else: print("tyvärr fel")
+
+färg = input("vilken färg gillar du?").capitalize()
+if färg == ("Blå"):
+    print ("helt rätt")
+    score +=2
+else: print("tyvärr fel")
+
+poäng = input("hur många poång fick du; vill dun se dina poång?").capitalize()
+if poäng == ("Ja"):
+    score
+print (score)
+
+
+
+
