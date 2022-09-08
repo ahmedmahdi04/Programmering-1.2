7aebb7aeedafd66b7639282b87cce7cfaafdba3b
Ahmed Mahdi <ahmed.mahdi@elev.ga.ntig.se>
2022-09-08 12:58:33 +0200

    rps

---
+++ rps.py
@@ -0,0 +1,36 @@
+import random
+
+computer = random.choice(["rock", "paper", "scissors"])
+
+user = input("rock, paper or scissors? ")
+
+print("The computer chose", computer,"and the user chose", user +".")
+
+# TODO - Implement the if statements that prints who wins
+
+if user == "rock" and computer == "paper":
+    print("computer wins")
+
+elif user == "paper" and computer == "scissors":
+    print("computer wins")
+
+elif user == "rock" and computer == "scissors":
+    print("user wins")
+
+elif user == "paper" and computer == "rock":
+    print("user wins")
+
+elif user == "rock" and computer == "rock":
+    print("equilibrium ")
+
+elif computer == "scissors" and user == "paper":
+    print("computer wins")
+
+elif computer == "scrissors" and user == "rock":
+    print("user wins")
+
+elif computer == "paper" and user == "scrissors":
+    print("user wins")
+
+elif user == "scrissors" and computer == "scrissors":
+    print("equilibrium ")
