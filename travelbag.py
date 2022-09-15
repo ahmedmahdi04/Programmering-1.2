1ea16da4810b354026cffdea9bbde29c8b10b548
Ahmed Mahdi <ahmed.mahdi@elev.ga.ntig.se>
2022-09-15 12:39:40 +0200

    travelbag done

---
+++ travelbag.py
@@ -0,0 +1,22 @@
+travelbag = ["Tröja", "Badbyxor", "Mobil", "Bok"]
+
+while True:
+       menyval = input("1. Kolla i resväskan\n"
+                   "2. Lägg sak i resväskan\n"
+                   "3. Ta bort sak i resväskan\n"
+                   "4. Avsluta program")
+
+       if menyval == "1":
+              print(travelbag)
+
+       elif menyval == "2":
+             Penna = input("vad vill du lägga i väskan?")
+             travelbag.append(Penna)
+
+       elif menyval == "3":
+              Mobil = input("vad vill ta bort från väskan?")
+              travelbag.remove(Mobil)
+
+       elif menyval == "4":
+              break
+              
